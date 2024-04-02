3e4188cb74abec13c870c0aad526d30a4b58fe8e
Cole Greer <112986082+Cole-Greer@users.noreply.github.com>
2024-04-02 16:36:58 -0700

    Add retries to queries with ConflictExceptions (#11)
    During testing, we've encountered some issues with ConflictException's being thrown. This PR adds simple retry logic for these errors.
    
    Also included is an extra change to enforce the minimum version of botocore with support for parameterized queries in Neptune Analytics.

--- nodestream_plugin_neptune/neptune_connection.py
+++ nodestream_plugin_neptune/neptune_connection.py
@@ -1,4 +1,6 @@
+import asyncio
 import json
+import random
 from abc import ABC, abstractmethod
 from logging import getLogger
 
@@ -10,26 +12,22 @@ class NeptuneConnection(ABC):
     def logger(self):
         return getLogger(self.__class__.__name__)
 
-    async def execute(self, query_stmt: str, parameters):
-        response = None
+    async def execute(self, query_stmt: str, parameters) -> dict | None:
+        response: dict | None = None
+        max_retries = 3
+        retry_delay = 1
+
         async with self._create_boto_client() as client:
             try:
-                response = await self._execute_query(
-                    client,
-                    query_stmt=query_stmt,
-                    parameters=parameters,
+                response = await self.__retry(
+                    func=lambda: self.__attempt_query(client, query_stmt, parameters),
+                    max_retries=max_retries,
+                    delay=retry_delay,
+                    exceptions=self._get_retryable_exceptions(client),
                 )
-
-                code = response["ResponseMetadata"]["HTTPStatusCode"]
-                if code != 200:
-                    self.logger.error(
-                        f"Query `{query_stmt}` failed with response:\n{response}"
-                    )
 
             except Exception as e:
-                self.logger.exception(
-                    f"Unexpected error for query: {query_stmt}.", e, stack_info=True
-                )
+                self.logger.exception(f"Unexpected error: {e} for query: {query_stmt}.")
 
         return response
 
@@ -38,7 +36,86 @@ class NeptuneConnection(ABC):
         pass
 
     @abstractmethod
-    async def _execute_query(self, client, query_stmt: str, parameters: str):
+    async def _execute_query(
+        self, client, query_stmt: str, parameters: str
+    ) -> dict | None:
+        pass
+
+    async def __attempt_query(
+        self, client, query_stmt: str, parameters: str
+    ) -> dict | None:
+        """
+        Attempts to execute OC query `query_stmt` with `parameters` via `client`
+
+        Args:
+            client:
+                Client to run query. Client is of whichever type is produced by `self._create_boto_client()`
+            query_stmt: str
+                OpenCypher query statement string to execute.
+            parameters: str
+                Query parameters encoded as a JSON string.
+
+        Returns: dict | None
+            The response from the query.
+
+        Raises:
+            Exception: Any exception thrown from the client when attempting queries.
+        """
+        response = await self._execute_query(
+            client,
+            query_stmt=query_stmt,
+            parameters=parameters,
+        )
+
+        code = response["ResponseMetadata"]["HTTPStatusCode"]
+        if code != 200:
+            self.logger.error(f"Query `{query_stmt}` failed with response:\n{response}")
+
+        return response
+
+    async def __retry(self, func, max_retries: int, exceptions, delay=0):
+        """
+        Retries a function `func` up to `attempts` times on encountering exceptions in `exceptions`.
+        Waits for `delay` seconds between retries.
+
+        Args:
+            max_retries: int
+                Number of retry attempts.
+            exceptions: (Exception...)
+                Exceptions to catch and retry on (tuple of exception types).
+            delay: int
+                Delay in seconds between retries.
+            func: function
+                Function to be retried.
+
+        Returns: dict | None
+            The return value of the successful function call.
+
+        Raises:
+            Exception: The last encountered exception if all retries fail.
+        """
+
+        for attempt in range(1, max_retries + 1):
+            try:
+                return await func()
+            except exceptions as e:
+                if attempt < max_retries:
+                    self.logger.warning(
+                        f"Query failed on attempt {attempt}/{max_retries} with error: {e} "
+                        f"Retrying in {delay}s."
+                    )
+                    await asyncio.sleep(delay + random.uniform(0, 0.5))
+                    delay *= 2
+                    continue  # retry on conflict exception
+                else:
+                    self.logger.exception(
+                        f"Query failed on attempt {attempt}/{max_retries} with error: {e} "
+                        f"Max retries reached."
+                    )
+                    raise e
+
+    @abstractmethod
+    def _get_retryable_exceptions(self):
         pass
 
 
@@ -46,9 +123,7 @@ class NeptuneDBConnection(NeptuneConnection):
     @classmethod
     def from_configuration(cls, host: str, graph_id: str = None, **client_kwargs):
         if host is None:
-            raise ValueError(
-                "A `host` must be specified when `mode` is 'database'."
-            )
+            raise ValueError("A `host` must be specified when `mode` is 'database'.")
         if graph_id is not None:
             raise ValueError(
                 "A `graph_id` should not be used with Neptune Database, `host=<Neptune Endpoint>` should be used "
@@ -56,7 +131,7 @@ class NeptuneDBConnection(NeptuneConnection):
             )
         return cls(host=host, **client_kwargs)
 
-    def __init__(self, host: str,  **client_kwargs) -> None:
+    def __init__(self, host: str, **client_kwargs) -> None:
         self.host = host
         self.boto_session = get_session()
         self.client_kwargs = client_kwargs
@@ -80,6 +155,9 @@ class NeptuneDBConnection(NeptuneConnection):
             # Use json.dumps() to warp dict's key/values in double quotes.
             parameters=json.dumps(parameters),
         )
+
+    def _get_retryable_exceptions(self, client):
+        return (client.exceptions.ConcurrentModificationException,)
 
 
 class NeptuneAnalyticsConnection(NeptuneConnection):
@@ -118,3 +196,6 @@ class NeptuneAnalyticsConnection(NeptuneConnection):
             language="OPEN_CYPHER",
             parameters=parameters,
         )
+
+    def _get_retryable_exceptions(self, client):
+        return (client.exceptions.ConflictException,)
